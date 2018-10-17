5aab62e92aebbd257999b4097ad339d5dbf736fe
jincod <vadim.abdrashitov@gmail.com>
2018-10-17 12:20:57 +0500

    updated packages

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -6,18 +6,15 @@ namespace Client
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             var valueServiceApi = RestService.For<IValueServiceRefitApi>("http://localhost:5000/api");
 
-            Task.Run(async () =>
-            {
-                await valueServiceApi.GetAllValues();
-                await valueServiceApi.GetValue("id1");
-                await valueServiceApi.CreateValue(null);
-                await valueServiceApi.UpdateValue("id1", null);
-                await valueServiceApi.DeleteValue("id1");
-            }).GetAwaiter().GetResult();
+            await valueServiceApi.GetAllValues();
+            await valueServiceApi.GetValue("id1");
+            await valueServiceApi.CreateValue(null);
+            await valueServiceApi.UpdateValue("id1", null);
+            await valueServiceApi.DeleteValue("id1");
         }
     }
 }
